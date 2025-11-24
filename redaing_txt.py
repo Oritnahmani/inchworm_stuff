2eb50cf9139f61974bf32a62ce378b7286e5120f
Oritnahmani <oritnahmani@mail.tau.ac.il>
2025-11-24 16:14:45 +0200

    save

--- redaing_txt.py
+++ redaing_txt.py
@@ -4,5 +4,22 @@ import scipy
 import matplotlib.pyplot as plt
 import scipy.constants
 import h5py
+from green_mbtools.pesto import mb
+from mbanalysis import ir
 
 
+def read_array_from_txt(number_of_orbitals):
+    data = np.array
+    for i in range(number_of_orbitals):
+        for j in range(number_of_orbitals):
+            data = np.loadtxt(f'/home/orit/VS_codes1/example/G_{i}_{j}.dat')
+    return data
+
+
+
+if __name__ == '__main__':
+    number_of_orbitals = 3
+    # for i in range(number_of_orbitals):
+    #     for j in range(number_of_orbitals):
+    # filename = f'/home/orit/VS_codes1/example/G_{i}_{j}.dat'
+    
