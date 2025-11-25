b12d53aea1a6ca227a41316f3070a2767acdca5c
Oritnahmani <oritnahmani@mail.tau.ac.il>
2025-11-25 18:11:29 +0200

    save

--- redaing_txt.py
+++ redaing_txt.py
@@ -8,8 +8,15 @@ from green_mbtools.pesto import mb
 from mbanalysis import ir
 
 
-def read_array_from_txt(number_of_orbitals):
-    data = np.array
+def read_greenfunction_from_txt(number_of_orbitals, time_filename):
+    with open(time_filename) as k:
+        for line in k:
+            line = line.strip()
+            if not line or line.startswith("#"):
+                continue
+            t_str, ij_str = line.split()
+            t_shape = len(t_str)
+    green = np.zeros((t_shape, number_of_orbitals, number_of_orbitals))
     for i in range(number_of_orbitals):
         for j in range(number_of_orbitals):
             # data = np.loadtxt(f'/home/orit/VS_codes1/example/G_{i}_{j}.dat')
@@ -20,16 +27,29 @@ def read_array_from_txt(number_of_orbitals):
                     if not line or line.startswith("#"):
                         continue
                     t_str, ij_str = line.split()            # split into 2 parts
-                    t = float(t_str)
-                    times.append(t)
-    return times
+                    green[:, i, j] = np.array([complex(x) for x in ij_str.strip().split(',')])
+    return green
+
+def read_delta_tau_from_txt(delta_file):
+    delta_tau = np.zeros((number_of_orbitals, number_of_orbitals))
+    with open(delta_file) as k:
+        for line in k:
+            line = line.strip()
+            if not line or line.startswith("#"):
+                continue
+            i , j , ij_str = line.split()
+            delta_tau[int(i), int(j)] = complex(ij_str)
+    return delta_tau
+           
 
 
 
 if __name__ == '__main__':
-    number_of_orbitals = 3
+    number_of_orbitals = 4
     # for i in range(number_of_orbitals):
     #     for j in range(number_of_orbitals):
-    # filename = f'/home/orit/VS_codes1/example/G_{i}_{j}.dat'
-    times = read_array_from_txt(number_of_orbitals)
-    print(times)
+    time_filename = f'/home/orit/VS_codes1/example/G_{0}_{0}.dat'
+    delta_file = '/home/orit/VS_codes1/example/delta.txt'
+    green = read_greenfunction_from_txt(number_of_orbitals, time_filename)
+    delta_tau = read_delta_tau_from_txt(time_filename)
+    # print(green)
