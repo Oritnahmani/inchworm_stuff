3a5d45d450b47b93940f81e6201139af2c589894
Oritnahmani <oritnahmani@mail.tau.ac.il>
2025-11-25 22:44:07 +0200

    save

--- redaing_txt.py
+++ redaing_txt.py
@@ -1,9 +1,9 @@
 import numpy as np
-import itertools
+# import itertools
 import scipy
 import matplotlib.pyplot as plt
 import scipy.constants
-import h5py
+# import h5py
 from green_mbtools.pesto import mb
 from mbanalysis import ir
 
@@ -21,14 +21,14 @@ def read_greenfunction_from_txt(number_of_orbitals, time_filename):
         for j in range(number_of_orbitals):
             # data = np.loadtxt(f'/home/orit/VS_codes1/example/G_{i}_{j}.dat')
             times = []
-            with open(f'/home/orit/VS_codes1/example/G_{i}_{j}.dat') as f:
+            with open(f'/home/orit/green_fun/inchworm/example/G_{i}_{j}.dat') as f:
                 for line in f:
                     line = line.strip()
                     if not line or line.startswith("#"):
                         continue
                     t_str, ij_str = line.split()            # split into 2 parts
                     green[:, i, j] = np.array([complex(x) for x in ij_str.strip().split(',')])
-    return green
+    return green_tau
 
 def read_delta_tau_from_txt(delta_file):
     delta_tau = np.zeros((number_of_orbitals, number_of_orbitals))
@@ -40,6 +40,10 @@ def read_delta_tau_from_txt(delta_file):
             i , j , ij_str = line.split()
             delta_tau[int(i), int(j)] = complex(ij_str)
     return delta_tau
+
+def fourier_trans_for_all(delta_tau,green_tau):
+    
+
            
 
 
@@ -48,8 +52,8 @@ if __name__ == '__main__':
     number_of_orbitals = 4
     # for i in range(number_of_orbitals):
     #     for j in range(number_of_orbitals):
-    time_filename = f'/home/orit/VS_codes1/example/G_{0}_{0}.dat'
-    delta_file = '/home/orit/VS_codes1/example/delta.txt'
+    time_filename = f'/home/orit/green_fun/inchworm/example/G_{0}_{0}.dat'
+    delta_file = '/home/orit/green_fun/inchworm/example/delta.txt'
     green = read_greenfunction_from_txt(number_of_orbitals, time_filename)
-    delta_tau = read_delta_tau_from_txt(time_filename)
+    # delta_tau = read_delta_tau_from_txt(time_filename)
     # print(green)
