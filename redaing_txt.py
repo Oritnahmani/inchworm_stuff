fb2433acb35692b7a65b51a31a4e59513214826c
Oritnahmani <oritnahmani@mail.tau.ac.il>
2026-01-05 18:32:52 +0200

    save

--- redaing_txt.py
+++ redaing_txt.py
@@ -8,7 +8,7 @@ from green_mbtools.pesto import mb
 from mbanalysis import ir
 
 
-def read_greenfunction_from_txt(number_of_orbitals, time_filename):
+def read_greenfunction_from_txt(number_of_orbitals, time_filename,green_path):
     t_list = []
     with open(time_filename) as k:
         for line in k:
@@ -16,7 +16,7 @@ def read_greenfunction_from_txt(number_of_orbitals, time_filename):
             if not line or line.startswith("#"):
                 continue
             # t_str, ij_str = line.split()
-            t_str = line.strip()
+            t_str = line.split()[0]
             t_list.append(float(t_str))
     t_arr = np.array(t_list)    
     t_shape = len(t_arr)
@@ -24,7 +24,7 @@ def read_greenfunction_from_txt(number_of_orbitals, time_filename):
     for i in range(number_of_orbitals):
         for j in range(number_of_orbitals):
             # data = np.loadtxt(f'/home/orit/VS_codes1/example/G_{i}_{j}.dat')
-            with open(f'/home/orit/VS_codes1/data_analyzing_from_mbpt/G_{i}_{j}.dat') as f:
+            with open(f'{green_path}/G_{i}_{j}.dat') as f:
                 for line in f:
                     line = line.strip()
                     if not line or line.startswith("#"):
@@ -55,9 +55,19 @@ def read_delta_tau_from_txt(delta_file,t_arr,number_of_orbitals):
             delta_tau[l, int(i), int(m)] = complex(float(ij_str_re) + 1j * float(ij_str_im))
     return delta_tau
 
-# def fourier_trans_for_all(delta_tau,green_tau):
     
-
+def read_hopping_from_txt(hopping_file, number_of_orbitals):
+    hopping = np.zeros(( number_of_orbitals, number_of_orbitals), dtype=complex)
+    with open(hopping_file) as k:
+        for raw_line in k:
+            line = raw_line.strip()
+            if not line or line.startswith("#"):
+                continue
+            i_str, m_str, ij_str_re, ij_str_im = line.split()
+            i = int(i_str)
+            m = int(m_str)
+            hopping[ int(i), int(m)] = complex(float(ij_str_re) + 1j * float(ij_str_im))
+    return hopping
            
 
 
@@ -68,8 +78,10 @@ if __name__ == '__main__':
     #     for j in range(number_of_orbitals):
     time_filename = f'/home/orit/VS_codes1/example/G_0_0.dat'
     delta_file = '/home/orit/VS_codes1/example/delta.txt'
-    green_tau, t_arr = read_greenfunction_from_txt(number_of_orbitals, time_filename)
+    hopping_file = '/home/orit/VS_codes1/example/hopping.txt'
+    green_tau, t_arr = read_greenfunction_from_txt(number_of_orbitals, time_filename,'/home/orit/VS_codes1/example')
     delta_tau = read_delta_tau_from_txt(delta_file, t_arr, number_of_orbitals)
-    print(delta_tau.shape)
+    hopping = read_hopping_from_txt(hopping_file, number_of_orbitals)
+    print (hopping)
     # print(t_arr.shape)
     # print(green_tau)
